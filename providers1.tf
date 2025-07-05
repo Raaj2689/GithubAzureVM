676ab8ecda0b6f76338ab14675d2ce6e5bca9ff8
Raaj2689 <raajkhamal@gmail.com>
2025-07-05 05:41:32 +0530

    modify providers.tf file

--- providers1.tf
+++ providers1.tf
@@ -7,11 +7,13 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  features {}
+# Remove or comment out the duplicate provider block below if you already have it in main.tf
+# If you want to specify credentials, use this format:
 
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
-}
+# provider "azurerm" {
+#   features {}
+#   client_id       = var.client_id
+#   client_secret   = var.client_secret
+#   tenant_id       = var.tenant_id
+#   subscription_id = var.subscription_id
+# }
